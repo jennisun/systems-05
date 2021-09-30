81805fc63923592d630513f3521f854e5c0344f1
jennisun <jsun20@stuy.edu>
2021-09-29 20:41:23 -0400

    steps 1 and 2

---
+++ pointer.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+  char a = 'a';
+  int b = 1;
+  long c = 1234;
+
+  // int *ia = &a;
+
+  printf("char a: %x\n", a);
+  printf("int b: %x\n", b);
+  printf("long c: %x\n", c);
+  return 0;
+}
