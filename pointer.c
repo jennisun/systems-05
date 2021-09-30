13f8969e6dd6065727c9e67ca2c0ced9cef7465f
jennisun <jsun20@stuy.edu>
2021-09-29 21:22:24 -0400

    3-6, reorganized

--- pointer.c
+++ pointer.c
@@ -1,14 +1,39 @@
 #include <stdio.h>
 
 int main() {
+
+  // 1
   char a = 'a';
   int b = 1;
   long c = 1234;
 
-  // int *ia = &a;
+  // 2
+  printf("char: %p, int: %p, long: %p\n", &a, &b, &c);
+  printf("char: %lu, int: %lu, long: %lu\n\n", &a, &b, &c);
 
-  printf("char a: %x\n", a);
-  printf("int b: %x\n", b);
-  printf("long c: %x\n", c);
+  // 3
+  char *cp = &a;
+  int *ip = &b;
+  long *lp = &c;
+
+  // 4
+  printf("char: %p, int: %p, long: %p\n\n", cp, ip, lp);
+
+  // 5
+  *cp = 'b';
+  *ip += 5;
+  *lp += 5;
+  printf("char: %c, int: %d, long: %ld\n\n", a, b, c);
+
+  // 6
+  unsigned int e = 10;
+  int *uip = &e;
+  char *uic = &e;
+
+  // 7
+  printf("uip: %p uip points to: %u\n", uip, *uip);
+  printf("uic: %p uic points to: %c\n\n", uic, *uic);
+
+
   return 0;
 }
